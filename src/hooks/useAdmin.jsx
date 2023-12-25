745f9257490b2803a23ef1b2c020431316c53ca1
Kasem32833 <abulkasem32833@gmail.com>
2023-12-25 20:17:08 +0300

    fix is admin issue

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -2,34 +2,19 @@ import { useQuery } from "@tanstack/react-query";
 import useAuth from "./useAuth";
 import useAxiosSecure from "./useAxiosSecure";
 
-const useAdmin = () => {
-    const {user}  = useAuth();
-    const axiosSecure = useAxiosSecure();
-    console.log(user);
-    let admin = false;
-    const [ data = 'isAdmin'] = useQuery({
-        queryKey: ['uasers'],
-        queryFn: ()=>{
-            axiosSecure.get(`/dashboard/users/${user.email}`)
-            .then(res => {
-                console.log(res.data);
-                const savedUser = res.data
-                if(savedUser.email = user.email){
-                    console.log('Uaser is an admin');
-                }
 
-            })
-            .catch(err=>{
-                console.log(err);
-            })
+const useAdmin = () => {
+    const { user } = useAuth();
+    const axiosSecure = useAxiosSecure();
+    const { data: isAdmin, isPending: isAdminLoading } = useQuery({
+        queryKey: [user?.email, 'isAdmin'],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/users/admin/${user.email}`);
+            console.log(res.data);
+            return res.data;
         }
-    })  
-    
-    return (
-        <div>
-            
-        </div>
-    );
+    })
+    return [isAdmin, isAdminLoading]
 };
 
 export default useAdmin;
--- src/pages/dashboard/addItems/Additems.jsx
+++ src/pages/dashboard/addItems/Additems.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import SectionTitle from "../../../shared/sectionTitle/SectionTitle";
 import { useForm } from "react-hook-form";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key{image_hosting_key}`
 
 const Additems = () => {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => {
+  const axiosPublic = useAxiosPublic();
+  const onSubmit = async (data) => {
     console.log(data);
+    // image upload to image bb and then get an url
+    const res = await  axiosPublic.post(image_hosting_api, )
+
   };
 
   return (
@@ -16,13 +24,14 @@ const Additems = () => {
       ></SectionTitle>
       <div>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <label>First Name</label>
+          <label>Recipi Name</label>
           <input {...register("name")} />
-          <label>Gender Selection</label>
+
+          <label>Category</label>
           
-          <select {...register("category")} className="select select-ghost w-full max-w-xs">
-            <option disabled selected>
-              Pick the best JS framework
+          <select defaultValue="default" {...register("category")} className="select select-ghost w-full max-w-xs">
+            <option disabled value="default">
+              Select Category
             </option>
             <option value="salad">Salad</option>
             <option value="pizza">Pizza</option>
